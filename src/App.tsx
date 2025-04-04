3ec488cccfbeb1221b54438d24f7fbc54da5c3aa
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-04 14:02:00 +0200

    feat: remove unnecessary imports from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import './App.css'
-import Onboarding from './app/Onboarding'
-// import Login from './app/Login'
-// import Signup from './app/Signup'
 
 function App() {
 
   return (
     <>
       <div>
-        {/* <Login/> */}
-        {/* <Signup/> */}
-        <Onboarding/>
+
       </div>
     </>
   )
