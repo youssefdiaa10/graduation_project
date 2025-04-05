b9a51834e8294cfc3fda6ef8c7d16111523977d3
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-06 00:37:14 +0200

    feat: delete comments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,17 +6,14 @@ const Navbar = () => {
     <div className="bg-white shadow-lg flex gap-2.5 py-10">
         <div className="ml-15 flex gap-5 items-center">
             <h1 className="font-medium">English</h1>
-            {/* <span>🌐</span> */}
             <IoEarth className="text-[22px]"/>
         </div>
         <p className="text-gray-600 font-bold mx-5">|</p>
         <div>
-            {/* <h1 className="font-medium">🔍</h1> */}
             <IoSearch  className="text-[22px]"/>
         </div>
         <p className="text-gray-600 font-bold mx-5">|</p>
         <div className="flex items-center gap-3">
-            {/* <span>⬇️</span> */}
             <IoIosArrowDown className="text-[22px]"/>
             <h1 className="font-medium">Genre (All)</h1>
         </div>
