ddc146cd2a71767997db6a2baaceaf169e1f8c6e
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-07 00:23:29 +0200

    feat: new animation to Blog 70% finished

--- src/app/Blog.tsx
+++ src/app/Blog.tsx
@@ -1,26 +1,31 @@
+import { useRef } from "react"
 import { BookItem } from "../components/BookItem"
 import { Infos } from "../components/Infos"
 import Navbar from "../components/Navbar"
 import { books } from "../utils/constants"
+import { useScroll, useTransform, motion } from "framer-motion"
 const Blog = () => {
   return (
-    <div>
+    <div className="h-screen">
       <div>
         <Navbar/>
       </div>
-      <div className="mt-10 grid grid-flow-col">
-        <div className="shadow-xl rounded-2xl">
+      <div className="mt-10 grid grid-flow-col grid-cols-5 h-screen">
+        <div className="shadow-xl rounded-2xl col-span-1 grid grid-cols-subgrid bg-white">
           <Infos />
         </div>
-        <div className="px-5 grid grid-cols-5 gap-y-8">
-          {books.map(book => (
-            <BookItem
-            image_src={book.image_src}
-            book_name={book.book_name}
-            author_name={book.author_name}
-            rating={book.rating}
-            />
-          ))}
+
+        <div className="col-span-4 grid-cols-subgrid ">
+            <div className="px-5 flex flex-row flex-nowrap gap-4 my-10 overflow-y-hidden overflow-x-scroll">
+              {books.map(book => (
+                <BookItem
+                image_src={book.image_src}
+                book_name={book.book_name}
+                author_name={book.author_name}
+                rating={book.rating}
+                />
+              ))}
+            </div>
         </div>
       </div>
     </div>
@@ -28,3 +33,30 @@ const Blog = () => {
 }
 
 export default Blog
+
+
+
+const HorizontalScrollCarousel = () => {
+  const targetRef = useRef<HTMLDivElement | null>(null);
+  const {scrollYProgress} = useScroll({
+    target: targetRef,
+  })
+  const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"])
+  return (
+    <>
+        <motion.div
+        style={{x}}
+        className="px-5 flex gap-4 bg-green-500 mt-10"
+        >
+        {books.map(book => (
+          <BookItem
+          image_src={book.image_src}
+          book_name={book.book_name}
+          author_name={book.author_name}
+          rating={book.rating}
+          />
+        ))}
+        </motion.div>
+    </>
+  )
+}
