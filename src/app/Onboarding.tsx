22c93bb79ea2ee469a5c21875bb46f1a5162c523
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-07 23:44:27 +0200

    feat: remove some unneeded code from Onboarding page

--- src/app/Onboarding.tsx
+++ src/app/Onboarding.tsx
@@ -1,5 +1,5 @@
 import { onboardingData } from "../utils/constants"
-import { Link } from "react-router-dom"
+import { Link, Links } from "react-router-dom"
 import { motion } from "framer-motion"
 import { slideInFromDown, slideInFromLeft, slideInFromRight, slideInFromTop } from '../utils/motion';
 
@@ -10,21 +10,31 @@ const Onboarding = () => {
     animate="visible"
     className="overflow-hidden"
     >
-        <div className="rounded-bl-[200px] rounded-br-[500px] py-6 bg-img-onboarding">
-            <motion.div
-            variants={slideInFromRight(1.5)}
-            className="py-3 flex justify-end">
-                <Link to={"/login"}>
-                    <button className="py-4 px-7 rounded-2xl text-2xl mr-9 text-white font-bold bg-main-color italic">Sign in</button>
-                </Link>
-            </motion.div>
+        <div className="rounded-bl-[500px] rounded-br-[500px] bg-img-onboarding py-20">
+
             <div className="flex flex-col items-center">
                 <motion.div variants={slideInFromTop(1)}>
-                    <h1 className="text-secondary-color text-2xl font-bold italic mb-6"><span className="text-[50px]">Unlimited Books, and more</span> <br /> Starts at $10</h1>
+                    <h1 className="text-secondary-color text-2xl font-bold italic mb-6"><span className="text-[50px]">Unlimited Books, and more</span> <br /></h1>
                     <h1 className="text-secondary-color text-2xl font-bold italic">Ready to Read? Enter your email/phone number <br /> to create or restart your membership.</h1>
                     <div className="mt-7 flex">
-                        <motion.input variants={slideInFromLeft(2)} type="text" className="rounded-2xl outline-none pl-4 pr-10 py-4 bg-main-color focus:text-white  placeholder:font-bold placeholder:italic placeholder:text-[16px] placeholder:text-gray-300" placeholder="Email address or Phone"/>
-                        <motion.button variants={slideInFromRight(2)} className="ml-5 italic bg-main-color text-white px-6 font-bold rounded-2xl">Get Started</motion.button>
+                        <Link to={"/login"}>
+                            <motion.button
+                            variants={slideInFromLeft(1.5)}
+                            whileHover={{backgroundColor: "#0261A0"}}
+                            style={{backgroundColor: "#003659"}}
+                            className="text-[30px] py-2 italic text-white px-6 font-bold rounded-2xl">
+                                Login
+                            </motion.button>
+                        </Link>
+                        <Link to={"/signup"}>
+                            <motion.button
+                            variants={slideInFromRight(1.5)}
+                            whileHover={{backgroundColor: "#0261A0"}}
+                            style={{backgroundColor: "#003659"}}
+                            className="ml-5 text-[30px] py-2 italic text-white px-6 font-bold rounded-2xl">
+                                Sign up
+                            </motion.button>
+                        </Link>
                     </div>
                 </motion.div>
             </div>
