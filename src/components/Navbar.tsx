40568e58829f674fa7d8194ed8d59fef1241ed7d
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-11 20:20:38 +0200

    feat: add accordion to genre

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
 import { IoIosArrowDown } from "react-icons/io"
 import { IoEarth, IoSearch } from "react-icons/io5"
+import { NavLink } from "react-router-dom"
+import { useGenreShowStore } from "../store/genreShowStore"
 
 const Navbar = () => {
+
+  const { setIsShow } = useGenreShowStore()
+
   return (
     <div className="bg-white shadow-lg flex gap-2.5 py-10">
         <div className="ml-15 flex gap-5 items-center">
@@ -9,12 +14,12 @@ const Navbar = () => {
             <IoEarth className="text-[22px]"/>
         </div>
         <p className="text-gray-600 font-bold mx-5">|</p>
-        <div>
+        <NavLink to={"/home/search"}>
             <IoSearch  className="text-[22px]"/>
-        </div>
+        </NavLink>
         <p className="text-gray-600 font-bold mx-5">|</p>
         <div className="flex items-center gap-3">
-            <IoIosArrowDown className="text-[22px]"/>
+            <IoIosArrowDown onClick={setIsShow} className="text-[22px]"/>
             <h1 className="font-medium">Genre (All)</h1>
         </div>
     </div>
