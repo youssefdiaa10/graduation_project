4b51194914db072b9ae0bfec4e2a55540111356f
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-04-12 19:11:35 +0200

    feat: add favorite to Book component

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -2,6 +2,8 @@ import { FaStar } from "react-icons/fa"
 import { IBook } from "../utils/types"
 import { Link } from "react-router-dom"
 import { useGenreShowStore } from "../store/genreShowStore"
+import { useFavoriteBooks } from "../context/FavoriteContext"
+import { IoHeartOutline, IoHeartSharp } from "react-icons/io5"
 
 export const Book = ({
     image_src,
@@ -11,6 +13,9 @@ export const Book = ({
 
         const { isShow, setIsShow } = useGenreShowStore()
 
+        const { addBookToFavorite, removeBookFromFavorite, isFavorite } = useFavoriteBooks()
+        const favorite = isFavorite(book_name)
+
         const handleAccordion = () => {
             if (isShow === true) {
                 setIsShow()
@@ -19,17 +24,37 @@ export const Book = ({
 
   return (
     <>
-        <Link onClick={handleAccordion} to={`/home/${book_name}`} className="shrink-0 bg-white my-2 p-4 shadow-xl cursor-pointer hover:scale-105 transition duration-300">
-            <img src={image_src} alt="book cover" width={150} height={150} className="rounded-2xl m-auto"/>
-            <div className="text-center mt-5">
+        <div className="shrink-0 flex flex-col gap-5 justify-between bg-white my-2 p-4 shadow-xl cursor-pointer hover:scale-105 transition duration-300">
+            <Link onClick={handleAccordion} to={`/home/${book_name}`} className="">
+                <img src={image_src} alt="book cover" width={150} height={150} className=" rounded-2xl m-auto"/>
+            </Link>
+            <div className="text-center">
                 <h1 className="text-[16px]">{book_name}</h1>
                 <h1 className="text-[13px] text-gray-600">{author_name}</h1>
-                <div className="flex gap-2 items-center justify-center mt-2">
-                    <span>{rating}</span>
-                    <FaStar className="text-yellow-300"/>
+                <div className="flex gap-8 items-center justify-center mt-2">
+                    <div className="flex gap-2 items-center">
+                        <span>{rating}</span>
+                        <FaStar className="text-yellow-300"/>
+                    </div>
+                    <div>
+                        {
+                            favorite ?
+                            <IoHeartSharp
+                            onClick={() => {
+                                removeBookFromFavorite(book_name)
+                            }}
+                            className="text-[35px]  text-red-500"/>
+                            :
+                            <IoHeartOutline
+                            onClick={() => {
+                                addBookToFavorite({ image_src, book_name, author_name, rating })
+                            }}
+                            className="text-[35px] text-black"/>
+                        }
+                    </div>
                 </div>
             </div>
-        </Link>
+        </div>
     </>
   )
 }
