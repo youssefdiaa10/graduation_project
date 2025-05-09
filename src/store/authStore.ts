b803a17dd34d50f3f155e433cb2564b50a82320e
youssefdiaa10 <103288467+youssefdiaa10@users.noreply.github.com>
2025-05-09 09:46:37 +0300

    feat: fix refresh issue

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -2,46 +2,56 @@ import { create } from "zustand"
 import { IRigesterResponse, IUser } from "../utils/types";
 import axios from "axios";
 
+import { persist } from 'zustand/middleware'
+
 type UserStore = {
     user: IUser | null;
     message: string;
     isAuth: boolean;
     emailConfirmationLink: string
-    signup: (email:string, phone:string, username:string, password:string) => Promise<void>;
+    signup: (email: string, phone: string, username: string, password: string) => Promise<void>;
     login: (email: string, password: string) => Promise<void>;
 }
 
-export const useUserStore = create<UserStore>((set) => (
-    {
-        user: null,
-        message: "",
-        isAuth: false,
-        emailConfirmationLink: "",
-        login: async (email, password) => {
-            const response = await axios.post("http://smartshelf.runasp.net/api/User/login", {
-                "email": email,
-                "Password": password
-            })
-            // console.log("Space")
-            // console.log(response.data.user)
-            // localStorage.setItem("isAuth", "true")
-            set({ isAuth: true, user: response.data.user})
-        },
-        signup: async (email, phone, username, password) => {
-            try {
-                const response = await axios.post<IRigesterResponse>("http://smartshelf.runasp.net/api/User/register",
-                { 
-                    "Email": email,
-                    "Phone": phone,
-                    "Username": username,
-                    "Password": password 
-                })
-                // console.log(response.data.emailConfirmationLink)
-                // console.log(response.data.message)
-            set({ message: response.data.message, emailConfirmationLink: response.data.emailConfirmationLink })
-            } catch (error) {
-                console.log(error)
+export const useUserStore = create<UserStore>()(
+    persist(
+        (set, get) => {
+            return {
+                user: null,
+                message: "",
+                isAuth: false,
+                emailConfirmationLink: "",
+                login: async (email, password) => {
+                    const response = await axios.post("http://smartshelf.runasp.net/api/User/login", {
+                        "email": email,
+                        "Password": password
+                    })
+                    // console.log("Space")
+                    // console.log(response.data.user)
+                    // localStorage.setItem("isAuth", "true")
+                    set({ isAuth: true, user: response.data.user })
+                },
+                signup: async (email, phone, username, password) => {
+                    try {
+                        const response = await axios.post<IRigesterResponse>("http://smartshelf.runasp.net/api/User/register",
+                            {
+                                "Email": email,
+                                "Phone": phone,
+                                "Username": username,
+                                "Password": password
+                            })
+                        // console.log(response.data.emailConfirmationLink)
+                        // console.log(response.data.message)
+                        set({ message: response.data.message, emailConfirmationLink: response.data.emailConfirmationLink })
+                    } catch (error) {
+                        console.log(error)
+                    }
+                },
             }
-        },
-    }
-))
+        }
+        ,
+        {
+            name: "auth-store"
+        }
+    )
+)
